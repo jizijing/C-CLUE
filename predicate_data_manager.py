818fca12e493a69c014e279fdd459ecd8b00a7b5
jizijing <jizijing@163.com>
2021-05-24 03:10:18 +0800

    Add files via upload

---
+++ predicate_data_manager.py
@@ -0,0 +1,127 @@
+import os
+import sys
+import json
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../bert")))
+import tokenization
+
+print("if not have raw data, please dowload data from http://lic2019.ccf.org.cn/kg !")
+
+def unzip_and_move_files():
+    "解压原始文件并且放入 raw_data 文件夹下面"
+    os.system("unzip dev_data.json.zip")
+    os.system("mv dev_data.json raw_data/dev_data.json")
+    os.system("unzip train_data.json.zip")
+    os.system("mv train_data.json raw_data/train_data.json")
+
+
+class Model_data_preparation(object):
+
+    def __init__(self, RAW_DATA_INPUT_DIR="raw_data", DATA_OUTPUT_DIR="classfication_data",
+                 vocab_file_path="vocab.txt", do_lower_case=True, Competition_Mode=False, Valid_Model=False):
+        '''
+        :param RAW_DATA_INPUT_DIR: 输入文件目录，一般是原始数据目录
+        :param DATA_OUTPUT_DIR: 输出文件目录，一般是分类任务数据文件夹
+        :param vocab_file_path: 词表路径，一般是预先训练的模型的词表路径
+        :param do_lower_case: 默认TRUE
+        :param Competition_Mode: 非比赛模式下，会把验证valid数据作为测试test数据生成
+        :param Valid_Model: 验证模式下，仅仅会生成test测试数据
+        '''
+        # BERT 自带WordPiece分词工具，对于中文都是分成单字
+        self.bert_tokenizer = tokenization.FullTokenizer(vocab_file=self.get_vocab_file_path(vocab_file_path),
+                                                         do_lower_case=do_lower_case)  # 初始化 bert_token 工具
+        self.DATA_INPUT_DIR = self.get_data_input_dir(RAW_DATA_INPUT_DIR)
+        self.DATA_OUTPUT_DIR = os.path.join(os.path.dirname(__file__), DATA_OUTPUT_DIR)
+        self.Competition_Mode = Competition_Mode
+        self.Valid_Model= Valid_Model
+        print("数据输入路径：", self.DATA_INPUT_DIR)
+        print("数据输出路径：", self.DATA_OUTPUT_DIR)
+        print("是否是比赛模式（非比赛模式下，会把验证valid数据作为测试test数据生成）：", self.Competition_Mode)
+        print("是否是验证模式（验证模式下，仅仅会生成test测试数据）：", self.Valid_Model)
+
+    # 获取输入文件路径
+    def get_data_input_dir(self, DATA_INPUT_DIR):
+        DATA_INPUT_DIR = os.path.join(
+            os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")), DATA_INPUT_DIR)
+        return DATA_INPUT_DIR
+
+    # 获取词汇表路径
+    def get_vocab_file_path(self, vocab_file_path):
+        vocab_file_path = os.path.join(
+            os.path.abspath(os.path.join(os.path.dirname(__file__), "../../pretrained_model/chinese_L-12_H-768_A-12")), vocab_file_path)
+        return vocab_file_path
+
+    # 处理原始数据
+    def separate_raw_data_and_token_labeling(self):
+        if not os.path.exists(self.DATA_OUTPUT_DIR):
+            os.makedirs(os.path.join(self.DATA_OUTPUT_DIR, "train"))
+            os.makedirs(os.path.join(self.DATA_OUTPUT_DIR, "valid"))
+            os.makedirs(os.path.join(self.DATA_OUTPUT_DIR, "test"))
+
+        file_set_type_list = ["train", "valid", "test"]
+        if self.Valid_Model:
+            file_set_type_list = ["test"]
+        for file_set_type in file_set_type_list:
+            print("produce data will store in: ", os.path.join(os.path.join(self.DATA_OUTPUT_DIR, file_set_type)))
+            if file_set_type in ["train", "valid"] or not self.Competition_Mode:
+                predicate_out_f = open(
+                    os.path.join(os.path.join(self.DATA_OUTPUT_DIR, file_set_type), "predicate_out.txt"), "w",
+                    encoding='utf-8')
+            text_f = open(os.path.join(os.path.join(self.DATA_OUTPUT_DIR, file_set_type), "text.txt"), "w",
+                          encoding='utf-8')
+            token_in_f = open(os.path.join(os.path.join(self.DATA_OUTPUT_DIR, file_set_type), "token_in.txt"), "w",
+                              encoding='utf-8')
+            token_in_not_UNK_f = open(
+                os.path.join(os.path.join(self.DATA_OUTPUT_DIR, file_set_type), "token_in_not_UNK.txt"), "w",
+                encoding='utf-8')
+
+            def predicate_to_predicate_file(spo_list):
+                predicate_list = [spo_list['predicate']]
+                predicate_list_str = " ".join(predicate_list)
+                predicate_out_f.write(predicate_list_str + "\n")
+
+            if file_set_type == "train":
+                path_to_raw_data_file = "train_data.json"
+            elif file_set_type == "valid":
+                path_to_raw_data_file = "dev_data.json"
+            else:
+                if self.Competition_Mode == True:
+                    path_to_raw_data_file = "test1_data_postag.json"
+                else:
+                    path_to_raw_data_file = "test_data.json"
+
+            with open(os.path.join(self.DATA_INPUT_DIR, path_to_raw_data_file), 'r', encoding='utf-8') as f:
+                count_numbers = 0
+                while True:
+                    line = f.readline()
+                    if line:
+                        count_numbers += 1
+                        r = json.loads(line)
+                        if (not self.Competition_Mode) or file_set_type in ["train", "valid"]:
+                            spo_list = r["spo_list"]
+                        else:
+                            spo_list = []
+                        text = r["text"]
+                        text_tokened = self.bert_tokenizer.tokenize(text)
+                        text_tokened_not_UNK = self.bert_tokenizer.tokenize_not_UNK(text)
+
+                        if (not self.Competition_Mode) or file_set_type in ["train", "valid"]:
+                            predicate_to_predicate_file(spo_list)
+                        text_f.write(text + "\n")
+                        token_in_f.write(" ".join(text_tokened) + "\n")
+                        token_in_not_UNK_f.write(" ".join(text_tokened_not_UNK) + "\n")
+                    else:
+                        break
+            print("all numbers", count_numbers)
+            print("\n")
+            text_f.close()
+            token_in_f.close()
+            token_in_not_UNK_f.close()
+
+if __name__ == "__main__":
+    RAW_DATA_DIR = "raw_data_guwen"
+    DATA_OUTPUT_DIR = "new_classification_data_guwen"
+    Competition_Mode = False
+    Valid_Mode = False
+    model_data = Model_data_preparation(
+        RAW_DATA_INPUT_DIR=RAW_DATA_DIR, DATA_OUTPUT_DIR=DATA_OUTPUT_DIR, Competition_Mode=Competition_Mode, Valid_Model=Valid_Mode)
+    model_data.separate_raw_data_and_token_labeling()
---
+++ prepare_data_for_labeling_infer.py
@@ -0,0 +1,83 @@
+import os
+
+# 获取最新模型预测数据文件夹
+def get_latest_model_predict_data_dir(new_epochs_ckpt_dir=None):
+    # 获取文件下最新文件路径
+    def new_report(test_report):
+        lists = os.listdir(test_report)  # 列出目录的下所有文件和文件夹保存到lists
+        lists.sort(key=lambda fn: os.path.getmtime(test_report + "/" + fn))  # 按时间排序
+        file_new = os.path.join(test_report, lists[-1])  # 获取最新的文件保存到file_new
+        return file_new
+    if new_epochs_ckpt_dir is None:
+        # 获取分类预测输出文件路径
+        input_new_epochs = os.path.join(
+                os.path.abspath(os.path.join(os.path.dirname(__file__), "../../output")), "predicate_infer_out")
+        # 获取最新周期文件路径
+        new_ckpt_dir = new_report(input_new_epochs)
+        input_new_epochs_ckpt = os.path.join(input_new_epochs, new_ckpt_dir)
+        # 获取最新周期下最新模型文件路径
+        new_epochs_ckpt_dir = new_report(input_new_epochs_ckpt)
+    return new_epochs_ckpt_dir
+
+#对于没有预测出关系的句子，启发式地选择相对概率最大的10个关系作为输出
+def replace_empty_infer_predicate_to_three_possible_values(predicate_score_value):
+    label_list = ['任职', '隶属于', '子', '同名于', '号', '作战', '位于', '依附', '名', '讨伐', '去往', '升迁', '杀', '管理','弟','出生地', '葬于', '属于', '父', '归属', '朋友', '兄', '作', '姓', '字']
+    predicate_score_value_list = predicate_score_value.split(" ")
+    predicate_score_name_value_list = [(label, value) for label, value in zip(label_list, predicate_score_value_list)]
+    predicate_score_name_value_sort_list = sorted(predicate_score_name_value_list, key=lambda x: x[1], reverse=True)
+    name_value_three_items = predicate_score_name_value_sort_list[:10]
+    three_predicate_list = [name for name, value in name_value_three_items]
+    return three_predicate_list
+
+
+def prepare_data_for_subject_object_labeling_infer(predicate_classifiction_input_file_dir,
+                                                   predicate_classifiction_infer_file_dir, out_file):
+    """
+    Converting the predicted results of the multi-label classification model
+    into the input format required by the sequential label model
+    :param predicate_classifiction_input_file_dir: Path of Input file of classification model
+    :param predicate_classifiction_infer_file_dir: Path of Predictive Output of Classification Model
+    :param out_file: Path of Input file of sequential labeling model
+    :return: Input file of sequential labeling model
+    """
+    text_file = open(os.path.join(predicate_classifiction_input_file_dir, "text.txt"),
+                     "r", encoding='utf-8').readlines()
+    token_in_file = open(os.path.join(predicate_classifiction_input_file_dir, "token_in.txt"),
+                         "r", encoding='utf-8').readlines()
+    token_in_not_UNK_file = open(os.path.join(predicate_classifiction_input_file_dir, "token_in_not_UNK.txt"),
+                         "r", encoding='utf-8').readlines()
+    new_epochs_ckpt_dir = get_latest_model_predict_data_dir(predicate_classifiction_infer_file_dir)
+    predicate_predict_file = open(os.path.join(new_epochs_ckpt_dir, "predicate_predict.txt"),
+                                  "r", encoding='utf-8').readlines()
+    predicate_score_value_file = open(os.path.join(new_epochs_ckpt_dir, "predicate_score_value.txt"),
+                                      "r", encoding='utf-8').readlines()
+    output_text_file_write = open(os.path.join(out_file, "text_and_one_predicate.txt"), "w", encoding='utf-8')
+    output_token_in_file_write = open(os.path.join(out_file, "token_in_and_one_predicate.txt"), "w", encoding='utf-8')
+    output_token_in_not_UNK_file_write = open(os.path.join(out_file, "token_in_not_UNK_and_one_predicate.txt"), "w", encoding='utf-8')
+    count_line = 0
+    count_empty_line = 0
+    count_temporary_one_predicate_line = 0
+    for text, token_in, token_in_not_UNK, predicate_predict, predicate_score_value in zip(text_file, token_in_file, token_in_not_UNK_file, predicate_predict_file, predicate_score_value_file):
+        count_line += 1
+        predicate_list = predicate_predict.replace("\n", "").split(" ")
+        if predicate_predict == "\n":
+            count_empty_line += 1
+            predicate_list = replace_empty_infer_predicate_to_three_possible_values(predicate_score_value)
+        for predicate in predicate_list:
+            count_temporary_one_predicate_line += 1
+            output_text_file_write.write(text.replace("\n", "") + "\t" + predicate + "\n")
+            output_token_in_file_write.write(token_in.replace("\n", "") + "\t" + predicate + "\n")
+            output_token_in_not_UNK_file_write.write(token_in_not_UNK.replace("\n", "") + "\t" + predicate + "\n")
+    print("empty_line: {}, line: {}, empty percentage: {:.2f}%".format(count_empty_line, count_line, (count_empty_line/count_line) *100))
+    print("temporary_one_predicate_line: ", count_temporary_one_predicate_line)
+    print("输入文件行数：", count_line)
+    print("转换成一个text 对应一个 predicate 之后行数变为：", count_temporary_one_predicate_line)
+
+
+if __name__=="__main__":
+    predicate_classifiction_input_file_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../predicate_classifiction/new_classification_data_guwen/test"))
+    #predicate_classifiction_infer_file_dir = "output/predicate_infer_out/epochs6/ckpt23000"
+    predicate_classifiction_infer_file_dir = None #None表示使用最新模型输出
+    out_file = os.path.abspath(os.path.join(os.path.dirname(__file__), "../subject_object_labeling/sequence_labeling_data_guwen/test"))
+    prepare_data_for_subject_object_labeling_infer(predicate_classifiction_input_file_dir,
+                                                   predicate_classifiction_infer_file_dir, out_file)
